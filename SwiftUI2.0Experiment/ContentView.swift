05e706fc523cd8694a088fb88ee9bdf2b41bfcaf
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-05-27 20:58:19 +0600

    Initial Commit

---
+++ SwiftUI2.0Experiment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI2.0Experiment
+//
+//  Created by Sopnil Sohan on 27/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI2.0Experiment/SwiftUI2_0ExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2_0ExperimentApp.swift
+//  SwiftUI2.0Experiment
+//
+//  Created by Sopnil Sohan on 27/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2_0ExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
